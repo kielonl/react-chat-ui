31473b27ef55a5d7947269b6cd63d0ead8e77235
unknown <niwelator2@gmail.com>
2022-07-11 15:34:57 +0200

    Fixed send to server && better css

--- src/container/LoginFrom.js
+++ src/container/LoginFrom.js
@@ -1,24 +1,41 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./LoginFrom.css";
-
+const url = "http://192.168.2.81:8080/Users";
 const NameForm = () => {
-  const postdata = () => {
-    const data = {
-      username: "eoeo",
-    };
-    axios.post("http://192.168.2.81:8080/Users", data).then((data) => {
-      console.log(data);
-    });
-  };
+  const [name, setName] = useState("");
+  const [adres, setUrl] = useState("");
 
+  const handleSumibt = async (e) => {
+    e.preventDefault();
+    try {
+      const resp = await axios.post(url, { name: name, adres: adres });
+      console.log(resp.data);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
   return (
     <div id="fromlog">
-      <label>
-        USER:
-        <input type={"text"} name="user" />
-      </label>
-      <button onClick={postdata}>Submit</button>
+      <div id="upertext">Super app chat</div>
+      <div id="log">Login</div>
+      <div id="int">
+        <input
+          type={"text"}
+          name="user"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type={"text"}
+          name="url"
+          value={adres}
+          onChange={(e) => setUrl(e.target.value)}
+        />
+      </div>
+      <div id="button">
+        <button onClick={handleSumibt}>Submit</button>
+      </div>
     </div>
   );
 };
