7a638d1f31bd4e05b97e9ad448c86971e7b3bc42
unknown <niwelator2@gmail.com>
2022-07-11 12:44:00 +0200

    send data to server

--- src/container/LoginFrom.js
+++ src/container/LoginFrom.js
@@ -1,29 +1,25 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./LoginFrom.css";
-class NameForm extends React.Component {
-  sends = (event) => {};
-  /*
-  axios = {
-    method: "post",
-    url: "/users",
-    data: {
-      name: this.state.user,
-      password: this.state.password,
-    },
+
+const NameForm = () => {
+  const postdata = () => {
+    const data = {
+      username: "eoeo",
+    };
+    axios.post("http://192.168.2.81:8080/Users", data).then((data) => {
+      console.log(data);
+    });
   };
-  */
-  render() {
-    return (
-      <div id="fromlog">
-        <form>
-          <label>
-            USER:
-            <input type={"text"} name="user" />
-          </label>
-          <button>Submit</button>
-        </form>
-      </div>
-    );
-  }
-}
+
+  return (
+    <div id="fromlog">
+      <label>
+        USER:
+        <input type={"text"} name="user" />
+      </label>
+      <button onClick={postdata}>Submit</button>
+    </div>
+  );
+};
 export default NameForm;
