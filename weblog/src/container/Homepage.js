cfc46780aee51ffbb6bcd60d33ed23faa0d1176f
unknown <niwelator2@gmail.com>
2022-07-13 11:53:43 +0200

    Add table with username&&dataTime from server with get

--- weblog/src/container/Homepage.js
+++ weblog/src/container/Homepage.js
@@ -1,19 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./LoginFrom.css";
+const axios = require("axios");
 const Home = () => {
+  const [data, setDate] = useState([]);
+  //get wyświetla dane użytkowników w divach
+  const url = "http://localhost:8080/users";
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((resp) => {
+        console.log("pobieranie danych", resp.data);
+        setDate(resp.data);
+      })
+      .catch((err) => console.log(err));
+  });
+
+  const arr = data.map((data, index) => {
+    return (
+      <tr>
+        <td>{data.username}</td>
+        <td>{data.dataTime}</td>
+      </tr>
+    );
+  });
+
   return (
     <div className="homepage">
       <h1>Home page</h1>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque,
-        officiis. Doloribus nulla assumenda nesciunt? Itaque officiis quas
-        dignissimos sint iste facere tenetur, error eveniet accusamus,
-        recusandae ratione vel cum nostrum!<br></br>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque,
-        officiis. Doloribus nulla assumenda nesciunt? Itaque officiis quas
-        dignissimos sint iste facere tenetur, error eveniet accusamus,
-        recusandae ratione vel cum nostrum!
-      </p>
+      <div>
+        <table>{arr}</table>
+      </div>
     </div>
   );
 };
