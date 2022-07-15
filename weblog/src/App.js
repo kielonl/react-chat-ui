994478faf2bea24bdb1aac3295465e10aaa09bdf
kielon <mosadowski23@gmail.com>
2022-07-15 11:00:29 +0200

    chat

--- weblog/src/App.js
+++ weblog/src/App.js
@@ -3,7 +3,7 @@ import NameForm from "./container/LoginFrom";
 import About from "./container/About";
 import Home from "./container/Homepage";
 import WebChat from "./container/Chat";
-import Chat from "./container/chat/Chat";
+import ChatPage from "./container/chat/Chat";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -13,8 +13,8 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/home" element={<Home />} />
         <Route path="/login" element={<NameForm />} />
-        <Route path="/chat1" element={<WebChat />} />
-        <Route path="/chat" element={<Chat />} />
+        <Route path="/chat" element={<WebChat />} />
+        <Route path="/chatPage" element={<ChatPage />} />
       </Routes>
     </Router>
   );
--- weblog/src/container/chat/Chat.js
+++ weblog/src/container/chat/Chat.js
@@ -1,37 +1,101 @@
 import React from "react";
 import "./chat.css";
-const Chat = () => {
+const ChatPage = () => {
   return (
     <div className="body">
-      <div className="left-bar">1</div>
+      <div className="left-bar">
+        <div className="user-list">
+          <img
+            src="http://ocen-piwo.pl/upload/192ff0523b0ef20d9c28b716c29a1a52.webp"
+            alt="romper"
+            className="prof-pic"
+          />
+          <div className="user">username</div>
+        </div>
+        <div className="user-list">
+          <img
+            src="http://ocen-piwo.pl/upload/192ff0523b0ef20d9c28b716c29a1a52.webp"
+            alt="romper"
+            className="prof-pic"
+          />
+          <div className="user">username</div>
+        </div>
+        <div className="user-list">
+          <img
+            src="http://ocen-piwo.pl/upload/192ff0523b0ef20d9c28b716c29a1a52.webp"
+            alt="romper"
+            className="prof-pic"
+          />
+          <div className="user">username</div>
+        </div>
+        <div className="user-list">
+          <img
+            src="http://ocen-piwo.pl/upload/192ff0523b0ef20d9c28b716c29a1a52.webp"
+            alt="romper"
+            className="prof-pic"
+          />
+          <div className="user">username</div>
+        </div>
+      </div>
 
       <div className="right-bar">
-        <div className="channel-info-bar">
-          <div class="channel-info-name">Channel Name</div>
-        </div>
-
+        <nav id="navbar">
+          <div className="navbarM">
+            <a href="http://localhost:3000/About">About</a>
+            <a href="http://localhost:3000/Chat">Channels</a>
+          </div>
+          <div className="navbarT">nazwa kanalu</div>
+          <div className="navSpacer"></div>
+          <div className="navbarL">
+            <a id="logout" href="http://localhost:3000/Login">
+              Logout
+            </a>
+          </div>
+        </nav>
         <div className="chat-window">
           <div className="messages">
-            <div className="message-block-not-my">nie moja wiadomosc</div>
-            <div className="message-block-my">moja wiadomosc</div>
-            <div className="message-block-not-my">
-              nie moja wiadomoscnie moja wiadomoscnie moja wiadomoscnie moja
-              wiadomoscnie moja wiadomoscnie moja wiadomosc
+            <div className="message-block message-block-my ">
+              moja wiadomosc
             </div>
-            <div className="message-block-my">moja wiadomosc</div>
-            <div className="message-block-not-my">nie moja wiadomosc</div>
-            <div className="message-block-my">moja wiadomosc</div>
-            <div className="message-block-not-my">nie moja wiadomosc</div>
-            <div className="message-block-my">moja wiadomosc</div>
-            <div className="message-block-not-my">nie moja wiadomosc</div>
-            <div className="message-block-my">moja wiadomosc</div>
-            <div className="message-block-not-my">nie moja wiadomosc</div>
-            <div className="message-block-my">moja wiadomosc</div>
-            <div className="message-block-not-my">nie moja wiadomosc</div>
-            <div className="message-block-my">moja wiadomosc</div>
-            <div className="message-block-not-my">nie moja wiadomosc</div>
-            <div className="message-block-my">moja wiadomosc</div>
-            <div className="message-block-not-my">nie moja wiadomosc</div>
+
+            <div className="message-block message-block-not-my">
+              nie moja wiadomosc
+            </div>
+            <div className="message-block message-block-my ">
+              moja wiadomosc
+            </div>
+
+            <div className="message-block message-block-not-my">
+              nie moja wiadomosc
+            </div>
+            <div className="message-block message-block-my ">
+              moja wiadomosc
+            </div>
+
+            <div className="message-block message-block-not-my">
+              nie moja wiadomosc
+            </div>
+            <div className="message-block message-block-my ">
+              moja wiadomosc
+            </div>
+
+            <div className="message-block message-block-not-my">
+              nie moja wiadomosc
+            </div>
+            <div className="message-block message-block-my ">
+              moja wiadomosc
+            </div>
+
+            <div className="message-block message-block-not-my">
+              nie moja wiadomosc
+            </div>
+            <div className="message-block message-block-my ">
+              moja wiadomosc
+            </div>
+
+            <div className="message-block message-block-not-my">
+              nie moja wiadomosc
+            </div>
           </div>
         </div>
         <div className="send-message">
@@ -44,4 +108,4 @@ const Chat = () => {
     </div>
   );
 };
-export default Chat;
+export default ChatPage;
