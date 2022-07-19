71ad228a8dfae5764ec5da339ce5f1b26c4f7c07
FilipP <niwelator2@gmail.com>
2022-07-19 14:57:37 +0200

    Last fix after rename files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import LoginFrom from "./container/chat/components/LoginFrom";
 import About from "./container/chat/components/About";
-import WebChat from "./container/chat/components/Homepage";
+import Homepage from "./container/chat/components/Homepage";
 import Chatpage from "./container/chat/components/Chat";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
@@ -15,7 +15,7 @@ function App() {
           element={<LoginFrom setUser={setUser} xd={"xd"} />}
         />
         <Route path="/about" element={<About />} />
-        <Route path="/home" element={<WebChat user={user} />} />
+        <Route path="/home" element={<Homepage user={user} />} />
         <Route path="/chat" element={<Chatpage />} />
       </Routes>
     </Router>
