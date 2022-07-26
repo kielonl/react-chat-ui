09ceaa83a0c5f88516379dedef2c3b7491e2d405
FilipP <niwelator2@gmail.com>
2022-07-26 13:46:17 +0200

    Chanege About.js/About.css make Responsive web

--- src/container/About.js
+++ src/container/About.js
@@ -5,9 +5,46 @@ import Author from "./components/Author.js";
 
 function About() {
   return (
-    <>
-      <div className="bodyweb">
+    <div className="main-about">
+      <div class="navbar-about">
         <Navbar></Navbar>
+      </div>
+      <section class="container-about">
+        <div class="card">
+          <div class="card-image card-1"></div>
+          <h2>Miłosz Stachlewski</h2>
+          <p>Backend</p>
+        </div>
+        <div class="card">
+          <div class="card-image card-2"></div>
+          <h2>Kielon</h2>
+          <p>Backend</p>
+        </div>
+        <div class="card">
+          <div class="card-image card-3"></div>
+          <h2>Filip Popławski</h2>
+          <p>Frontend</p>
+        </div>
+        <div class="card">
+          <div class="card-image card-4"></div>
+          <h2>Oskar Kotlarski</h2>
+          <p>Backend</p>
+        </div>
+      </section>
+      <footer className="footer-about">
+        <h4>Website created: June of 2022</h4>
+      </footer>
+    </div>
+  );
+}
+export default About;
+/*
+  
+*/
+/*
+  <>
+      <div className="bodyweb">
+        
         <link rel="preconnect" href="https://fonts.googleapis.com"></link>
         <link
           rel="preconnect"
@@ -62,9 +99,5 @@ function About() {
         </footer>
       </div>
     </>
-  );
-  // {
-  //   this.state.persons.map((person) => <li key={person.id}>{person.name}</li>);
-  // }
-}
-export default About;
+
+*/
